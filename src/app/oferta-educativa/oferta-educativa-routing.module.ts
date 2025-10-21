ea9d4a9220f11dbd6b7097a927387fcd30b306bf
TheJulioHD <moranjulio78@gmail.com>
2025-10-20 18:29:38 -0700

    cambios

--- src/app/oferta-educativa/oferta-educativa-routing.module.ts
+++ src/app/oferta-educativa/oferta-educativa-routing.module.ts
@@ -4,11 +4,33 @@ import { ElectricaComponent } from './electrica/electrica.component';
 import { HomeComponent } from './maestria/components/home/home.component';
 import { AdminComponent } from './maestria/admin/admin.component';
 import { ObjetivoComponent } from './maestria/components/objetivo/objetivo.component';
+import { AeronacuticaComponent } from './aeronacutica/aeronacutica.component';
+import { BiomedicaComponent } from './biomedica/biomedica.component';
+import { ElectronicaComponent } from './electronica/electronica.component';
+import { SemiconductoresComponent } from './semiconductores/semiconductores.component';
+import { IndustrialComponent } from './industrial/industrial.component';
+import { MecanicaComponent } from './mecanica/mecanica.component';
+import { MecatronicaComponent } from './mecatronica/mecatronica.component';
+import { SistemasComponent } from './sistemas/sistemas.component';
+import { InformaticaComponent } from './informatica/informatica.component';
+import { GestionComponent } from './gestion/gestion.component';
 
 const routes: Routes = [
   {path:'',
     children: [
+      { path: 'Aeronautica', component: AeronacuticaComponent },
+      { path: 'Biomedica', component: BiomedicaComponent },
       { path: 'Electrica', component: ElectricaComponent },
+      { path: 'Electronica', component: ElectronicaComponent },
+      { path: 'Semiconductores', component: SemiconductoresComponent },
+      { path: 'Industrial', component: IndustrialComponent },
+      { path: 'Mecanica', component: MecanicaComponent },
+      { path: 'Mecatronica', component: MecatronicaComponent },
+      { path: 'Administracion', component: AdminComponent },
+      { path: 'Sistemas', component: SistemasComponent },
+      { path: 'Informatica', component: InformaticaComponent },
+      { path: 'Gestion', component: GestionComponent },
+      
       { path:'Maestria-Admin', component: AdminComponent},
       { path:'Objetivos', component: ObjetivoComponent}
     ]
